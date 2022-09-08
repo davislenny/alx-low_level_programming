1ab09380c3e7c6797cc61e44db0ab9f68bc32571
davislenny <davidlmwangi7@gmail.com>
2022-09-08 12:57:53 -0700

    using printf in C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/*
+ * Main - Entry point
+ *
+ * Description: Print "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
