fa87824ab599a4d1e6861a2eba5625c5e68f14cd
davislenny <davidlmwangi7@gmail.com>
2022-09-08 13:49:21 -0700

    print computer sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - the entry point
+ * main - entrypoint
  *
  * description: Print the size of various types of computer a C program runs on
  *
@@ -10,9 +10,9 @@ int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
 	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));j
 	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
-	printf("Size of a long int: %d btye(s)\n", (int) sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 
 	return (0);
 }
