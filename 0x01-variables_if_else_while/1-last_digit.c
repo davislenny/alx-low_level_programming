3cf64f94fd4e431615f47f1304a9379ca869e442
davislenny <davidlmwangi7@gmail.com>
2022-09-10 01:35:12 -0700

    declare the size of n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,14 +12,14 @@ int main(void)
 	int n;
 
 	srand(time(0));
-
 	n = rand() - RAND_MAX / 2;
 	
 	if (n > 5)
-		printf("The last digit of n is %d and is greater than 5," n);
+		printf("The last digit of n is %d and is greater than 5\n", n);
 	if (n == 0)
-		printf("The last digit of n is %d and is 0," n);
+		printf("The last digit of n is %d and is 0\n", n);
 	if (n < 6 &&!= 0)
-		printf("The last digit of n is %d and less than 6 and not 0," n);
+		printf("The last digit of n is %d and less than 6 and not 0\n", n);
+
 	return (0);
 }
