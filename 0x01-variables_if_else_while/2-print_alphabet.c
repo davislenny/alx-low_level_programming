413706a9b9d37ffa9a785eb68e4d2c730f918550
davislenny <davidlmwangi7@gmail.com>
2022-09-10 02:35:08 -0700

    using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char  alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar (alpha);
+		putchar ('\n');
+
+	retun (0);
+}
