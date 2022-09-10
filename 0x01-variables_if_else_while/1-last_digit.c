2e9ad32db3e07d44f264df668f15cc2c19106af7
davislenny <davidlmwangi7@gmail.com>
2022-09-10 01:45:19 -0700

    Print size of last n digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,13 +13,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if (n > 5)
-		printf("The last digit of n is %d and is greater than 5\n", n);
-	if (n == 0)
-		printf("The last digit of n is %d and is 0\n", n);
-	if (n < 6 &&!= 0)
-		printf("The last digit of n is %d and less than 6 and not 0\n", n);
+	last = n % 10;
+
+	if (last > 5)
+		printf("The last digit of %d is %d and is greater than 5\n", n, last);
+	if (last == 0)
+		printf("The last digit of %d is %d and is 0\n", n, last);
+	if (last < 6 && last != 0)
+		printf("The last digit of %d is %d and less than 6 and not 0\n", n, last);
 
 	return (0);
 }
