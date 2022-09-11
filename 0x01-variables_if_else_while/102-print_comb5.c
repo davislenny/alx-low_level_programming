1af672422b276ef31f597655d32f8c0a1ca8caca
davislenny <davidlmwangi7@gmail.com>
2022-09-11 01:11:39 -0700

    2 by2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,7 +21,7 @@ int main(void)
 	{
 		for (c = '0'; c <= '9'; c++)
 		{
-		for (c >= d ; c <= d)
+ 
 		for (d = '1'; d <= '9'; d++)
 		{
 			putchar(a);
@@ -30,7 +30,7 @@ int main(void)
 			putchar(c);
 			putchar(d);
 
-			if (a != '8' || b != '7' || c != '8' || d != '8')
+			if (a != '8' || b != '7' || c != '8' || d != '8' || c >= d ||)
 			{
 				putchar(44);
 				putchar(32);
