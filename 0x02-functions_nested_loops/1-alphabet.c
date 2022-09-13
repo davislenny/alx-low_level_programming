0be5f65eea75b8390b204fcb3964da3378d4063c
davislenny <davidlmwangi7@gmail.com>
2022-09-13 12:26:04 -0700

    print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,10 @@
 #include<stdio.h>
 /**
  * main - Entry point
- * Description: Print alphabets
- * Return: Always 0 (Success)
  *
+ * Descuription: Print alphabets
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
