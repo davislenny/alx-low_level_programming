6d3a7beb516385fdede4ace2f3efa0f49c3fe9a3
davislenny <davidlmwangi7@gmail.com>
2022-09-13 12:23:22 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: Print alphabets
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
