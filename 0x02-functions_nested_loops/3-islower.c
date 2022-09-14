6e11d45886f41fb752fc6de6217e559a28341158
davislenny <davidlmwangi7@gmail.com>
2022-09-14 05:02:09 -0700

    check for lowercase alphabets

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,14 @@
 /**
  * _islower - Check for lowercase alphabets
  *
- * Return: 1 if c is lowercase
- * Return: 0 otherwise
+ * Return: 1 if c is lowercase otherwise 0
+ * 
  */
 
 int _islower(int c)
 {
+	char c;
+
 	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
