5e70bf45adbb49f0719d7457b4822d19b71111b9
davislenny <davidlmwangi7@gmail.com>
2022-09-14 14:09:00 -0700

    p

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,11 +13,10 @@ void times_table(void)
 	{
 		for (c = 0; c <= 9; c++)
 		{
-			p = r * c;
-			_putchar(0);
 			_putchar(',');
 			_putchar(' ');
 
+			p = r * c;
 			if (p <= 9)
 			{
 				_putchar(' ');
