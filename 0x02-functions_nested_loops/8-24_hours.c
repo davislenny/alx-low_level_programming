06a56828c608643a2570d829e200c451b424c26b
davislenny <davidlmwangi7@gmail.com>
2022-09-14 12:02:04 -0700

    every minute of the day of Jack Bauer, starting from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,39 +7,28 @@
  */
 void jack_bauer(void)
 {
-	int h0, h1, m0, m1, hmax;
+	int a, b, c, d;
 
-	hmax = 58;
-	h0 = '0';
-	while (h0 < '3')
+	for (a = '0'; a <= '2'; a++)
 	{
-		if (h0 == '2')
+		for (b = '0'; b <= '9'; b++)
 		{
-			hmax = '4';
-		}
-		h1 = '0';
-		while (h1 < hmax)
-		{
-			m0 = '0';
-			while (m0 < '6')
+			for (c = '0'; c <= '5'; c++)
 			{
-				m1 = '0';
-				while (m1 < 58)
+				for (d = '0'; d <= '9'; d++)
 				{
-				_putchar(h0);
-				_putchar(h1);
-				_putchar(58);
-				_putchar(m0);
-				_putchar(m1);
-				m1++;
+					_putchar(a);
+					_putchar(b);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+
+					if((a != '2' || b != '3') && (c != '5' || d != '9'))
+					{
+						_putchar('\n');
+					}
 				}
-				m1 = '0';
-				m0++;
 			}
-			m0 = '0';
-			h1++;
 		}
-		h1 = '0';
-		h0++;
 	}
 }
