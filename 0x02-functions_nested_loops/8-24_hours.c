bf9b53e4bf5c1e7b7326ba6486d93e147f9aa1ad
davislenny <davidlmwangi7@gmail.com>
2022-09-14 09:53:29 -0700

    t

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ void jack_bauer(void)
 	{
 		for (b = '0'; b <= '9'; b++)
 		{
-			for (c = '0'; c <='5'; c++)
+			for (c = '0'; c <= '5'; c++)
 			{
 				for (d = '0'; d <= '9'; d++)
 				{
@@ -22,7 +22,7 @@ void jack_bauer(void)
 					_putchar(58);
 					_putchar(c);
 					_putchar(d);
-					if (a != '2' && b != '3' && c != '5' && d != '9')
+					if (a != '2' || b != '3' || c != '5' || d != '9')
 					{
 						_putchar('\n');
 					}
