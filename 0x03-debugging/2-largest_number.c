d0fca8db246388e9a89710a42da68ec89ac249c4
davislenny <davidlmwangi7@gmail.com>
2022-09-15 08:27:40 -0700

    Print largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,13 +2,9 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- *
  * @a: first integer
- *
  * @b: second integer
- *
  * @c: third integer
- *
  * Return: largest number
  */
 
@@ -16,23 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
-
-		printf("%d is the largest number\n", largest);
 	}
-	else if (b > a && a > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
-
-		printf("%d is the largest number\n", largest);
 	}
 	else 
 	{
 		largest = c;
-
-		printf("%d is the largest number\n", largest);
 	}
 
 	return (largest);
