487916305ffe6c40f62e25c42571a2ef178ea730
davislenny <davidlmwangi7@gmail.com>
2022-09-15 10:31:15 -0700

    Fix the print_remaining_days()

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,9 +10,9 @@
  * Return: void
  */
 
-int print_remaining_days(int month, int day, int year)
+void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 ||  year % 400 == 0) && !( year % 100 == 0))
+	if ((year % 4 == 0 ||  year % 400 == 0) && ( year % 400 != 0 || year % 4 != 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
@@ -21,17 +21,13 @@ int print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
+	else if (month == 2 && day == 60)
+	{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+	}
 	else
 	{
-		if (month == 2 && day == 60)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-			printf("Remaining days: %d\n", 365 - day);
-		}
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		printf("Remaining days: %d\n", 365 - day);
 	}
-	return (0);
 }
