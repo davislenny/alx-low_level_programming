57992b62183397ac30b8756beb399b797799a6c3
davislenny <davidlmwangi7@gmail.com>
2022-09-19 13:31:09 -0700

    a function that copies the string from a pointed variable to another

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,17 +1,35 @@
 #include "main.h"
 /**
- * *_strcpy - copies the string 
+ * _strlen - Returns length of string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (*s != '\0')
+	{
+		lenght++;
+		s++;
+	}
+	return (length);
+}
+
+/**
+ * *_strcpy - copies the string
  * @dest: a pointer variable
  * @src: a pointer variable
  * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	while (*src)
+	int i;
+
+	for (i = 0; i < _strlen(src); i++)
 	{
-		*dest = *src;
-		src++;
-		dest++;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
