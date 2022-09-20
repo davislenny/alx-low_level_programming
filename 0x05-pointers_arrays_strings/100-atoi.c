402b0e09ab79b49daa59b89809b503eaff81225a
davislenny <davidlmwangi7@gmail.com>
2022-09-20 03:39:27 -0700

    p

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,81 @@
+#include "main.h"
+/**
+ * _strlen - Returns string length
+ * @s: Poniter to the string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		lengh++;
+		s++;
+	}
+	return (length);
+}
+
+/**
+ * find_num - returs integer location
+ * @s: The string
+ * Return: integer locatio
+ */
+int find_num(char *s)
+{
+	int i;
+
+	for (i = 0; i <_strlen(s); i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			return (i);
+	}
+	return (-1);
+}
+/**
+ * find_sign - return integer value
+ * @s: integer string
+ * Return: sign
+ */
+int find_sign(char *s)
+{
+	int neg = 0, i = 0, sign = 1;
+
+	while (i < (find_num(s)))
+	{
+		if (s[i++] == '-')
+			neg++;
+	}
+	if (neg % 2 != 0)
+		sign = -1;
+	return (sign);
+}
+/**
+ * _atoi - convert string to int
+ * @s: the string
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int sign, dig, t, i, dig2;
+	unsigned int num = 0;
+	t = 1;
+	dig = 0;
+	dig2 = find_num(s);
+
+	if (find_num(s) < 0)
+		return (0);
+	sign = find_sign(s);
+	while ((s[find_num(s)] >= '0' && s[find_num(s)] <= '9') && (find_num(s) <= _strlen(s)))
+	{
+		dig += 1;
+		find_num(s)++;
+	}
+	i = 1;
+	for (i = dig2; i < (dig2 + dig); i++)
+	{
+		num += (s[i] - '0') * t;
+		t /= 10;
+	}
+	return (num * sign);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,5 +14,8 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int find_num(char *s);
+int find_sign(char *s);
 
 #endif /* MAIN_H */
