1c73c3433a925e40917237373a738cd8f4188e1d
davislenny <davidlmwangi7@gmail.com>
2022-09-23 05:38:59 -0700

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
- #include "main.h"
+ #include <stdio.h>
 
 /**
  * main - Entry point
@@ -7,13 +7,6 @@
  */
 int main(void)
 {
-	char c[9] = "_putchar";
-	int i = 0;
-
-	for (i = 0; i < 8; i++)
-	{
-		putchar(c[i]);
-	}
-	putchar('\n');
+	printf("_putchar\n");
 	return (0);
 }
