9fbd2a2bb7de2078dce1c7984d4e2ea273d84c34
davislenny <davidlmwangi7@gmail.com>
2022-09-26 03:22:25 -0700

    a function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * strchr -  locates a character in a string
+ * @s: the string
+ * @c: the fist located character
+ * Return: a pointer to the character
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return *(s + i);
+		}
+		else
+			return (NULL);
+	}
+}
