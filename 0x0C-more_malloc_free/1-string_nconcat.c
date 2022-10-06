1a45854c526abd88051fd267df2dd9253ffc1f49
davislenny <davidlmwangi7@gmail.com>
2022-10-06 14:22:15 -0700

    A function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,20 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * _strlen - find string length
+ * @str: string
+ * Return: length
+ */
+int _strlen(char *str)
+{
+	int length = 0;
+
+	while (*str++)
+		length++;
+	return (length);
+}
+
+/**
  * string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
@@ -9,32 +23,24 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j k;
-	char *pr;
+	char *pr, *pr1;
+	unsigned int i, j;
 
 	if (!s1)
-		i = 0;
-	else
-	{
-		for (i = 0; s1[i]; i++)
-			;
-	}
+		s1 = "";
 	if (!s2)
-		j = 0;
-	else
-	{
-		for (j = 0; s2[j]; j++)
-			;
-	}
-	if (j > n)
-		j = n;
-	pr = malloc(sizeof(char) * (i + j + 1));
-	if (!pr)
+		s2 = "";
+	i = _strlen(s1);
+	j = _strlen(s2);
+
+	j = j > n ? n : j;
+	pr = pr1 = malloc(i + j + 1);
+	if (!pr1)
 		return (NULL);
-	for (k = 0; k < j; k++)
-		pr[k] = s1[k];
-	for (k = 0; k < j; k++)
-		pr[k + i] = s2[k];
-	pr[i + j] = '\0';
+	while (*s1)
+		*pr1++ = *s1++;
+	while (j--)
+		*pr1++ = *s2++;
+	*pr1 = '\0';
 	return (pr);
 }
