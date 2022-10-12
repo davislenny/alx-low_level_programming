34d7caf8e5b9d8494209a2d059424039ee8ccd28
davislenny <davidlmwangi7@gmail.com>
2022-10-11 22:41:52 -0700

    A function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,6 +11,7 @@ char *_strdup(char *str)
 {
 	unsigned int i = 0;
 	char *s;
+
 	if (!str)
 		return (NULL);
 	while (*str++)
@@ -32,7 +33,7 @@ char *_strdup(char *str)
  * @owner: dog owner
  * Return: pointer to dog info
  */
-dog_t *new_dog(char *name, float age char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	dot_t *pr = malloc(sizeof(dog_t));
 
