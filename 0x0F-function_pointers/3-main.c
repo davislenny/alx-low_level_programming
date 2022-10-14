eea5d90ddd4d7f0d08ab76d95e18a33d86d962ff
davislenny <davidlmwangi7@gmail.com>
2022-10-14 06:13:28 -0700

    Main operation function

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,13 +18,13 @@ int main(int argc, char **argv)
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	funct = get_op_funck(argv[2]);
+	func = get_op_func(argv[2]);
 	if (!func)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (!b && (argv[2][0] == '/' || argv[2][0] == '/'))
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
 		exit(98);
