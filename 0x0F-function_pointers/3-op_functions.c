a6889b461d7abe49eda5e0c87c54f0f281a59285
davislenny <davidlmwangi7@gmail.com>
2022-10-14 05:48:43 -0700

    Operation functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * op_add - sum of a and b
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: sum
+ */
+int op_add(int a, int )
+{
+	return (a + b);
+}
+/**
+ * op_sub - difference of a and b
+ * @a: 1st num
+ * @b: 2nd num
+ * Return: diff
+ */
+int op_sub (int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies a and b
+ * @a: 1st num
+ * @b: 2nd num
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides a by b
+ * @a: 1st num
+ * @b: 2nd num
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - find remainder
+ * @a: 1st num
+ * @b: 2nd num
+ * Return: remainder of division
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
