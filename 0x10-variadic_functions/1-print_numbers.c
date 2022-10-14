9fd5ff17247e22dbb40b6fd63f41cc9d286db6aa
davislenny <davidlmwangi7@gmail.com>
2022-10-14 10:38:17 -0700

    A unction that prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers
+ * @separator: string between numbers
+ * @n: number of integers
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = n;
+	va_list args;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(args, n);
+	while (i--)
+		printf("%d%s", va_arg(args, int),
+				i ? (separator ? separator : " ") : "\n");
+	va_end(args);
+}
