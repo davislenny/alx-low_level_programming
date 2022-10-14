bfdf9821f418c8b9d2f68c1ebd7610629780f07a
davislenny <davidlmwangi7@gmail.com>
2022-10-14 11:06:10 -0700

    A function that prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,7 +3,6 @@
  * print_numbers - prints numbers
  * @separator: string between numbers
  * @n: number of integers
- * @...: argument integers
  * Return: void
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
@@ -20,6 +19,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	while (i--)
 		printf("%d%s", va_arg(args, int),
-				i ? (separator ? separator : " ") : "\n");
+				i ? (separator ? separator : "") : "\n");
 	va_end(args);
 }
