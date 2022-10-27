7057f7246ae0637fa924edf5de01f516217b255c
davislenny <davidlmwangi7@gmail.com>
2022-10-27 10:47:33 -0700

    p

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
-		return (-1);
+		return (1);
 	}
 	temp = *head;
 	while (temp)
@@ -33,5 +33,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		node = temp;
 		temp = temp->next;
 	}
-	return (-1);
+	return (1);
 }
