bf9b0fc26f816f7fdcccf4a0966221b2299f48f0
davislenny <davidlmwangi7@gmail.com>
2022-11-23 09:23:11 -0800

    A functions that converst a string to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,11 +8,25 @@
 
 int _atoi(char *s)
 {
+	int i, sign = 1. flag = 0, output;
+	unsigned int result = 0;
 
-	while (*s)
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
-		if (*s <= '9' && *s != '\0')
-			return (*s++);
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
 	}
-	return (0);
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+	return (output);
 }
