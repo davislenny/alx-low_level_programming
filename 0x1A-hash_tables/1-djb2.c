ee01cfce41bbaa43306d6bf912454e6df7901d6d
davislenny <davidlmwangi7@gmail.com>
2023-01-08 01:15:04 -0800

    Add 1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -2,9 +2,9 @@
 
 /**
  * hash_djb2 - Implementation of the djb2 algorithm
- * @str: String used to generate hash index
+ * @str: String used to generate hash value
  *
- * Return: Hash index
+ * Return: Hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
