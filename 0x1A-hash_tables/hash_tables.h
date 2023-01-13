7cc87479176cff8b876ad2ef53020afb319fb58a
davislenny <davidlmwangi7@gmail.com>
2023-01-13 11:49:40 -0800

    Add header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -80,11 +80,14 @@ typedef struct shash_table_s
 	shash_node_t *stail;
 } shash_table_t;
 
+/* The '100-sorted_hash_table' module */
 shash_table_t *shash_table_create(unsigned long int size);
 shash_node_t *screate_item(const char *key, const char *value);
 void shash_table_sort(shash_table_t *ht, shash_node_t *snode);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
+
+/* The '2-key_index' module */
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
