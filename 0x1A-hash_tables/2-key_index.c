4d259e7a370ffded85d3b2d8a317327b1473b952
davislenny <davidlmwangi7@gmail.com>
2023-01-13 11:46:54 -0800

    Add 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -12,3 +12,92 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 		return (0);
 	return (hash_djb2(key) % size);
 }
+
+
+/**
+ * shash_table_print - Prints the sorted hash table
+ * @ht: the table
+ * Return: void
+ */
+void shash_table_print(const shash_table_t *ht)
+{
+	unsigned long int index = 0;
+	shash_node_t *items;
+	char *flag = "";
+
+	if (!ht || !ht->array)
+		return;
+
+	printf("{");
+	while (index < ht->size)
+	{
+		items = ht->array[index];
+		while (items)
+		{
+			printf("%s'%s': '%s'", flag, items->key, items->value);
+			flag = ", ";
+			items = items->snext;
+		}
+		index++;
+	}
+	printf("}\n");
+}
+
+/**
+ * shash_table_print_rev - Prints table items in reverse
+ * @ht: the table
+ * Return: void
+ */
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	unsigned long int index = 0;
+	shash_node_t *items;
+	char *flag = "";
+
+	if (!ht)
+		return;
+
+	printf("{");
+	while (index < ht->size)
+	{
+		items = ht->array[index];
+		while (items)
+		{
+			printf("%s'%s': '%s'", flag, items->key, items->value);
+			flag = ", ";
+			items = items->sprev;
+		}
+		index--;
+	}
+	printf("}\n");
+}
+
+/**
+ * shash_table_delete - deletes the table
+ * @ht: the table
+ * Return: void
+ */
+void shash_table_delete(shash_table_t *ht)
+{
+	unsigned long int index = 0;
+	shash_node_t *items, *temp;
+
+	if (ht)
+	{
+		while (index < ht->size)
+		{
+			items = ht->array[index];
+			while (items)
+			{
+				temp = items;
+				items = items->next;
+				free(temp->key);
+				free(temp->value);
+				free(temp);
+			}
+			index++;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
